08ec8d66122451c6b3a8ed3f3364be6e8a8b8567
Lucia Cappellini <lucia.cappellini@iboux.com>
2024-12-02 19:20:56 -0300

    content

--- FE/src/api/index.js
+++ FE/src/api/index.js
@@ -4,7 +4,11 @@ axios.defaults.withCredentials = true;
 const API_URL = `${import.meta.env.VITE_API_URL}`;
 
 export async function iniciarJuego() {
-    return axios.get(`${API_URL}/iniciar`)
+    return axios.get(`${API_URL}/iniciar`, {
+        headers: {
+            'Content-Type': 'application/json', 
+        },
+        })
         .then(response => response.data)
         .catch(error => {
             console.error("Error al iniciar el juego:", error.response?.data || error.message);
@@ -13,7 +17,11 @@ export async function iniciarJuego() {
 }
 
 export async function registrarJugador(jugadorData) {
-    return axios.post(`${API_URL}/registrarse`, jugadorData)
+   return axios.post(`${API_URL}/registrarse`, jugadorData, {
+        headers: {
+            'Content-Type': 'application/json', 
+        },
+        })
         .then(response => response.data)
         .catch(error => {
             console.error("Error al registrar jugador:", error.response?.data || error.message);
@@ -22,5 +30,14 @@ export async function registrarJugador(jugadorData) {
 }
 
 export async function arriesgarLetra(letra) {
-  return axios.post(`${API_URL}/adivinar`, { letra }).then(response => response.data);
+    return axios.post(`${API_URL}/adivinar`, { letra }, {
+        headers: {
+            'Content-Type': 'application/json', 
+        },
+    })
+    .then(response => response.data)
+    .catch(error => {
+        console.error("Error al arriesgar letra:", error.response?.data || error.message);
+        throw error;
+    });
 }
